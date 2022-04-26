637c919ed0fdd129a2e86fd1486d3c783a73583f
Juan Camilo <j.fallag@uniandes.edu.co>
2022-04-26 14:38:42 -0500

    modulos org y patr.

---
+++ src/app/organizador/organizador.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrganizadorComponent } from './organizador.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [OrganizadorComponent]
+})
+export class OrganizadorModule { }
---
+++ src/app/patrocinador/patrocinador.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PatrocinadorComponent } from './patrocinador.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PatrocinadorComponent]
+})
+export class PatrocinadorModule { }
