72cce4c558e9eb47ce96963ffee72bf256e97515
Juan Camilo <j.fallag@uniandes.edu.co>
2022-04-28 15:01:03 -0500

    organizador.ts

---
+++ src/app/organizador/organizador.ts
@@ -0,0 +1,13 @@
+export class Organizador{
+  id: number;
+  nombre: string;
+  correo: string;
+  telefono: string;
+
+  constructor(id: number, nombre: string, correo: string, telefono: string){
+    this.id=id;
+    this.nombre=nombre;
+    this.correo = correo;
+    this.telefono = telefono;
+  }
+}
