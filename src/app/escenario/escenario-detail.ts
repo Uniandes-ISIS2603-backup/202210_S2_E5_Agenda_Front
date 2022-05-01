282eced5097a5c5a5b7a77a0f7c9c30e443e904e
Alejandro1400 <a.salgadom@uniandes.edu.co>
2022-05-01 13:30:06 -0500

    Detail Escenario y Evento

--- src/app/escenario/escenario-detail.ts
+++ src/app/escenario/escenario-detail.ts
@@ -1,4 +1,18 @@
 import { Escenario } from "./escenario";
+import { Multimedia } from "../multimedia/multimedia";
+import { Evento } from "../evento/evento";
 
 export class EscenarioDetail extends Escenario{
+
+  multimedias: Array<Multimedia> = [];
+  eventos: Array<Evento> = [];
+
+
+  constructor(id: number, nombre: string, tipo: string, aforoPermitido: number, multimedias: Array<Multimedia>, eventos: Array<Evento>)
+  {
+    super(id, nombre, tipo, aforoPermitido);
+    this.multimedias = multimedias;
+    this.eventos = eventos;
+  }
+
 }
--- src/app/multimedia/multimedia.ts
+++ src/app/multimedia/multimedia.ts
@@ -1,15 +1,23 @@
+import { Escenario } from "../escenario/escenario";
+import { Evento } from "../evento/evento";
+
+
 export class Multimedia {
 
   id : number;
   nombre : string;
   tipo : string;
   direccion : string;
+  escenario: Escenario;
+  evento: Evento;
 
-  constructor(id: number, nombre: string, tipo: string, direccion: string){
+  constructor(id: number, nombre: string, tipo: string, direccion: string, escenario: Escenario, evento: Evento){
     this.id=id;
     this.nombre=nombre;
     this.tipo = tipo;
     this.direccion = direccion;
+    this.escenario = escenario;
+    this.evento = evento;
   }
 
 }
