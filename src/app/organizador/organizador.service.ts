1896a22a34469d1ec69c4ce38a9f85fc60adc9be
JuanCFalla <j.fallag@uniandes.edu.co>
2022-05-03 15:07:24 -0500

    servicios JCF

---
+++ src/app/organizador/organizador.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Organizador } from './organizador';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrganizadorService {
+
+
+
+  constructor(private http: HttpClient) { }
+
+  getOrganizadores(): Observable<Organizador[]> {
+    return this.http.get<Organizador[]>('http://localhost:8080/organizadores');
+  }
+}
