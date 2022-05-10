1332dc15bd4501a4a6f3c10161159ee239b8e16f
santyxd3000 <s.pardom@uniandes.edu.co>
2022-05-10 14:48:39 -0500

    cambiosResponsable4

--- src/app/responsable/responsable.module.ts
+++ src/app/responsable/responsable.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResponsableListComponent } from './responsable-list/responsable-list.component';
+import { ResponsableDetailComponent } from './responsable-detail/responsable-detail.component';
 
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports: [ResponsableListComponent],
-  declarations: [ResponsableListComponent]
+  exports: [ResponsableListComponent,ResponsableDetailComponent],
+  declarations: [ResponsableListComponent, ResponsableDetailComponent]
 })
 export class ResponsableModule { }
